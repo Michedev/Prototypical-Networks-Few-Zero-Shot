7e7bd482dbca7ffd1ab1e3714865f383376b91ab
mikedev <mik3dev@gmail.com>
2020-08-10 00:31:27 +0200

    removed old functions and add seed_everything

--- train.py
+++ train.py
@@ -1,22 +1,22 @@
-from dataset import MiniImageNetDataLoader
-from model import PrototypicalNetwork
-from paths import ROOT, OMNIGLOTFOLDER, WEIGHTSFOLDER, EMBEDDING_PATH
+import fire
 import pytorch_lightning as pl
 from pytorch_lightning import seed_everything
-from pytorch_lightning.callbacks import ModelCheckpoint, ProgressBar
-import fire
-import torch
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from dataset import MiniImageNetDataLoader
+from model import PrototypicalNetwork
+from paths import WEIGHTSFOLDER, EMBEDDING_PATH
 
 
 def main(dataset, train_n: int, val_n: int, test_n: int, n_s: int, n_q: int, epochs: int = 1000, batch_size: int = 32,
          lr: float = 10e-3, trainsize: int = 10000, valsize: int = 64, testsize: int = 64,
-         force_download=False, early_stop=False, gpu=0):
+         force_download=False, early_stop=False, gpu=0, seed=42):
     assert dataset in ['omniglot', 'miniimagenet']
+    seed_everything(seed)
     EMBEDDING_PATH.replace('embedding', 'embedding_' + dataset)
     if dataset == 'omniglot':
         raise NotImplementedError("Omniglot not yet implemented")
     else:
-        pull_data_miniimagenet(force_download)
         datamodule = MiniImageNetDataLoader(batch_size, train_n, val_n, test_n, n_s, n_q, trainsize, valsize, testsize)
     checkpoint = ModelCheckpoint(
         WEIGHTSFOLDER / 'best_model', verbose=True,
